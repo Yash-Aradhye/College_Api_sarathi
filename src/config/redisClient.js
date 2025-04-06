910d799ec8985f27aeb09e85e1bc2fede289888e
MayankChandratre1 <mayankmchandratre@gmail.com>
2025-04-06 20:05:56 +0530

    added redis url

--- src/config/redisClient.js
+++ src/config/redisClient.js
@@ -4,7 +4,7 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const redis = new Redis({
+const redis = process.env.REDIS_URL ? new Redis(process.env.REDIS_URL) :new Redis({
   host: process.env.REDIS_HOST || '127.0.0.1',
   port: process.env.REDIS_PORT || 6379,
   password: process.env.REDIS_PASSWORD || ''
